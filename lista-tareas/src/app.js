9faba2986c6e02021d3dfb7c0c06841991f6b519
Cesar Trujillo <catgx55@gmail.com>
2023-09-24 20:24:46 -0500

    corrigiendo errores

--- lista-tareas/src/app.js
+++ lista-tareas/src/app.js
@@ -8,15 +8,10 @@ const port = 8080;
 
 app.use('/tareas', routerTarea, router, valueMethod);
 
-app.get("/tareas", (req, res) => {
-    res.
-    res.status(200).json("Bienvenido a la lista de tareas");
-});
-
 app.use((req, res) => {
     res.status(400).json("Ruta no encontrada");
 })
 
 app.listen(port, () => {
-    console.log(`El servidor esta en http://localhost:${port}/tareas`);
+    console.log(`El servidor esta en http://localhost:${port}`);
 });
